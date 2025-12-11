ffe6d2cedf0b37132e3319ea125ebc437cbd0e85
unknown <rafipramudya164@gmail.com>
2025-12-11 17:43:27 +0700

    push

---
+++ begin()/external.js
@@ -0,0 +1,5 @@
+function keluaran(nama,umur) {
+    return `halo nama saya ${nama} dan umur saya ${umur}`
+}
+
+module.exports = keluaran;
---
+++ begin()/internal.js
@@ -0,0 +1,6 @@
+const nama = "rasyadbahar";
+const umur = "19";
+
+const req = require("./external.js");
+
+console.log(req(nama,umur));
