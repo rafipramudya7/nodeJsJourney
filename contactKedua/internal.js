b6a6a45f3821e7bf36c4de4979e1875db56e32e6
unknown <rafipramudya164@gmail.com>
2025-12-15 00:56:25 +0700

    yargs

--- contactKedua/internal.js
+++ contactKedua/internal.js
@@ -2,6 +2,17 @@ const { resolve } = require('dns');
 const { stdin, stdout } = require('process');
 const rl = require('readline');
 const fs = require('fs');
+const yargs = require('yargs/yargs');
+const { type } = require('os');
+const { hideBin } = require('yargs/helpers');
+const { coerce } = require('yargs');
+
+
+const argv = yargs(hideBin(process.argv));
+// cek file
+if(!fs.existsSync('./data.json')){
+    fs.writeFileSync('./data.json','[]','utf-8');
+}
 
 const pertanyaan = rl.createInterface({
     input:stdin,
@@ -17,12 +28,9 @@ const ujian = (soal) => {
 
     })
 }
-// cek file
-if(!fs.existsSync('./data.json')){
-    fs.writeFileSync('./data.json','[]','utf-8');
-}
 
-const store = (dataInput) =>{
+
+const store = (dataInput) =>{ 
     var dataUtama = fs.readFileSync('./data.json','utf-8');
     dataUtama = JSON.parse(dataUtama);
 
@@ -30,6 +38,47 @@ const store = (dataInput) =>{
     dataUtama = JSON.stringify(dataUtama);
     fs.writeFileSync('./data.json',dataUtama);
 }
+
+argv.command({
+    command : 'tambah',
+    describe : 'sebuah fungsi untuk memasukan data',
+    builder : {
+        nama :{
+            type:'string',
+            demandOption:true,
+            coerce:(val)=>{
+                if(!val || val.trim()===''){
+                  throw new Error('nama tidak boleh kosonh cok');
+                }
+                return val;
+            }
+        },
+        email : {
+            type : 'string',
+            demandOption:false
+        },
+        phone:{
+            type:'string',
+            demandOption:true,
+             coerce:(val)=>{
+                if(!val || val.trim()===''){
+                  throw new Error('phone tidak boleh kosonh cok');
+                }
+                return val;
+            }
+        }
+    },
+    handler(argv){
+        const isi = {
+            nama : argv.nama,
+            email : argv.email,
+            phone: argv.phone
+        }
+        store(isi);
+        pertanyaan.close();
+    }
+    
+})
 
 const main = async()=>{
     const  nama = await ujian("siapa namamu : ");;
@@ -42,4 +91,10 @@ const main = async()=>{
 
 }
 
-main();
+if (process.argv.length <= 2) {
+    // tidak ada command → mode interaktif
+    main();
+} else {
+    // ada command → yargs
+    argv.help().parse();
+}
