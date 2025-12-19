34f01c746350214c2bdf52b39c624a55900f3f2a
unknown <rafipramudya164@gmail.com>
2025-12-19 22:02:57 +0700

    dasaranExpress

--- expressWebServer/internal.js
+++ expressWebServer/internal.js
@@ -1,19 +1,18 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
-const port = 8000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use((req, res, next) => {
-  if (!req.path.includes('.') && req.path !== '/') {
-    req.url += '.html';
-  }
-  next();
-});
+app.get('/:page',(req,res)=>{
+  const alur = path.join(__dirname,`${req.params.page}.html`);
+  res.sendFile(alur,(err)=>{
+    if(err) return res.status(404).sendFile(path.join(__dirname,'error.html'));
+  })
+})
 
-app.use(express.static(path.join(__dirname, "expressWebServer")));
-
-app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, "expressWebServer", "error.html"));
-});
+app.listen(3000,()=>{
+  console.log("server sedang berjalan...");
+})
