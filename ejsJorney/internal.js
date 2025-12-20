47074406121234b34c58368606da8091747a6940
unknown <rafipramudya164@gmail.com>
2025-12-20 22:32:40 +0700

    usingEjsLoop

--- ejsJorney/internal.js
+++ ejsJorney/internal.js
@@ -1,17 +1,41 @@
-import ejs from 'ejs';
-import express from 'express';
-
+import ejs from "ejs";
+import express from "express";
 
 const app = express();
-app.set('view engine','ejs');
-app.get('/:page',(req,res)=>{
-    const alur = req.params.page;
-    res.render(alur,(err,html)=>{
-        if(err)return res.render('error');
-        return res.render(alur)
-    });
-})
+app.set("view engine", "ejs");
+app.get("/:page", (req, res) => {
+  const alur = req.params.page;
+  const mahasiswa = [
+    {
+      nama: "aziz",
+      umur: "12",
+    },
+    {
+      nama: "umar",
+      umur: "13",
+    },
+    {
+      nama: "rumek",
+      umur: "18",
+    },
+  ];
+  res.render(
+    alur,
+    {
+      nama: "sano",
+      tittle: "ujiCoba",
+      mahasiswa
+    },
+    (err, html) => {
+      if (err) {
+        console.log("error");
+        return res.render("error");
+      }
+      res.send(html);
+    }
+  );
+});
 
-app.listen(3000,()=>{
-    console.log("server sedang berjalan...");
-})
+app.listen(3000, () => {
+  console.log("server sedang berjalan...");
+});
